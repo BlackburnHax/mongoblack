30bb4a83225390bf2e5fcc00fa2671dc476b70a6
Brandon Blackburn <contact@bhax.net>
2021-07-20 18:06:01 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os.path
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="mongoblack",
+    version="0.0.2",
+    description="MongoDB handlers to streamline application interfaces",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlackburnHax/mongoblack",
+    author="Brandon Blackburn",
+    author_email="contact@bhax.net",
+    license="Apache 2.0",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["mongoblack"],
+    include_package_data=True,
+    install_requires=["pymongo"],
+)
